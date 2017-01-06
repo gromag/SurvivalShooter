3443c5718bc2f6cf4de1ac57f2a83cd51765d745
Giuseppe Romagnuolo <gromag@gmail.com>
2017-01-06 00:23:29 +0000

    First commit of all code, added PlayerMovement, PlayerAC and Floor.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake(){
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animation> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxisRaw ("Horizontal"); 
+		float v = Input.GetAxisRaw ("Vertical");
+	}
+
+	void Move(float h, float v){
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning(){
+
+		Camera camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+	}
+}
